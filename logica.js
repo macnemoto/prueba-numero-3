da174ec32f64a3ad300b84eb21697f01768f7f3c
marcos pirela <marcosj465@gmail.com>
2020-06-24 21:06:42 -0400

    v3

--- logica.js
+++ logica.js
@@ -1,22 +1,38 @@
 'use strict'
 const cheerio = require('cheerio');
 const request = require('request-promise');
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
+
+//Api ress
 const app = express()
+const port = process.env.PORT || 3008
 
-app.listen(3000, () => {
-    console.log('Api Ya anda corriendo en http://localhost:3000');
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+     console.log(`Api Ya anda corriendo en http://localhost:${port}`);
 })
+
+
+
+
+
+app.get('/hispachan/:board/res/:th', (req, res) => {
+        let thId = req.params.th.split('.')[0];
+        let board = req.params.board;
+
+
+
+
 
 //funcion para ser una llamada al hilo y pasarsela a cheerio y este analizarla en "$"
-async function nekoMinig() {
+ async function nekoMinig() {
 
-    const $ = await request({
-    uri: 'https://www.hispachan.org/ve/res/1700357.html',
-    transform: body => cheerio.load(body)
-    
-})
+const $ =  await request({uri:`https://www.hispachan.org/${board}/res/${thId}.html`,
+    transform: body => cheerio.load(body)})
 //Obtener la imagen principal de hilos
 const pirure = $('img').attr('src');
 console.log(pirure);
@@ -32,6 +48,7 @@ console.log(i, $(el).text())
 
 
 
-}
+};
 //Ejecutando la funcion nekoMinig para que se inicie con el scrip
-//nekoMinig()
+ });
+nekoMinig()
