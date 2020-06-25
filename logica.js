f2290740a246e5feb80f0b8b2bfd9e1eb81a9259
Marcos <marcosj465@gmail.com>
2020-06-25 10:29:19 -0400

    v4

--- logica.js
+++ logica.js
@@ -5,9 +5,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
+
 //Api ress
 const app = express()
-const port = process.env.PORT || 3008
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
@@ -17,38 +18,80 @@ app.listen(port, () => {
 })
 
 
+ app.get('/hispachan.org/:board/res/:th.html', function(req, res) {
+    let thId = req.params.th.split('.')[0];
+     let board = req.params.board;
+  
+ 
 
 
 
-app.get('/hispachan/:board/res/:th', (req, res) => {
-        let thId = req.params.th.split('.')[0];
-        let board = req.params.board;
 
+//app.get('/hispachan/:board/res/:th', (req, res) => {
+  //      let thId = req.params.th.split('.')[0];
+    //    let board = req.params.board;
 
+    //});
 
 
 
 //funcion para ser una llamada al hilo y pasarsela a cheerio y este analizarla en "$"
- async function nekoMinig() {
 
-const $ =  await request({uri:`https://www.hispachan.org/${board}/res/${thId}.html`,
-    transform: body => cheerio.load(body)})
+async function nekoMaster() {
+
+
+
+await request(`https://www.hispachan.org/${board}/res/${thId}.html`, (err, resp, body) => {
+  if (res.statusCode == 404) {
+    // El hilo está en 404.
+    parseResponse(req, res, { status: 404 });
+    return;
+}
+if (err) {
+    // Otro error
+    parseResponse(req, res, { status: 500 });
+    return;
+}
+   
+  let $ = cheerio.load(body);
 //Obtener la imagen principal de hilos
-const pirure = $('img').attr('src');
-console.log(pirure);
-//Obtener el titulo del hilo
+
 const titulo = $('span .filetitle').first();
-console.log(titulo.text());
-//Obtener los comentarios de los negros del chon
+const pirure = $('img').attr('src');
+//const titulo = $('span .filetitle').first();
+//const titulo = $('span .filetitle').first();
+//console.log(pirure);
+
+//res.status(200).send(titulo.text());
 const post =  $('blockquote').each((i, el) => {
-console.log(i, $(el).text())
+  res.status(404 >= 100 && 404 < 600 ? err.code : 500);
+  res.send(i, $(el).text())
+//res.send(i, $(el).text());
+ });
+//res.status(200).send(pirure);
+
+//Obtener el titulo del hilo
+//const titulo = $('span .filetitle').first();
+//res.send(titulo);
+//console.log(titulo.text());
+//Obtener los comentarios de los negros del chon
+//const post =  $('blockquote').each((i, el) => {
+//console.log(i, $(el).text())
+//res.send(i, $(el).text());
+ //});
+
+
+
+});
+ 
+
+
+
 
 }
-);
+nekoMaster()
+});
 
 
-
-};
 //Ejecutando la funcion nekoMinig para que se inicie con el scrip
- });
-nekoMinig()
+//nekoMinig()
